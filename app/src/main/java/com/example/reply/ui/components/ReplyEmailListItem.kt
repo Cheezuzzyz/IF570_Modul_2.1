c604159af8869756564b5857c6783a05c3225d1a
Dimas.Bagus <dimasbagus.dbn@gmail.com>
2025-02-12 14:29:33 +0700

    final

--- app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
+++ app/src/main/java/com/example/reply/ui/components/ReplyEmailListItem.kt
@@ -102,15 +102,16 @@ fun ReplyEmailListItem(
             )
 
             Text(
-                text = email.subject,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
+                text = email.createdAt,
+                style = MaterialTheme.typography.labelMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
             )
 
             Text(
                 text = email.body,
                 maxLines = 2,
                 style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
                 overflow = TextOverflow.Ellipsis
             )
         }
